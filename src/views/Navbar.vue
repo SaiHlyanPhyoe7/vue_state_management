fcd644c6d0c06710a809d677591262e7dcc4ae58
Sai Hlyan Phyoe <sai843hp@prestigein.com>
2024-02-27 03:19:06 +0630

    Fix:UI CSS

--- src/views/Navbar.vue
+++ src/views/Navbar.vue
@@ -4,10 +4,10 @@
       <div class="container mx-auto flex justify-between items-center">
         <router-link to="/" class="text-2xl font-bold">Vue 3</router-link>
         <div>
-          <router-link to="/" class="mx-2">First Example</router-link>
-          <router-link to="/second" class="mx-2">Second Example</router-link>
-          <router-link to="/fix-all-condition" class="mx-2">fix-all-condition</router-link>
-          <router-link to="/mock-example" class="mx-2">mock-example</router-link>
+          <router-link to="/" class="mx-2 hover:underline underline-offset-4 decoration-sky-400 duration-500 hover:text-sky-400 p-4">First Example</router-link>
+          <router-link to="/second" class="mx-2 hover:underline underline-offset-4 decoration-sky-400 duration-500 hover:text-sky-400 p-4">Second Example</router-link>
+          <router-link to="/fix-all-condition" class="mx-2 hover:underline underline-offset-4 decoration-sky-400 duration-500 hover:text-sky-400 p-4">fix-all-condition</router-link>
+          <router-link to="/mock-example" class="mx-2 hover:underline underline-offset-4 decoration-sky-400 duration-500 hover:text-sky-400 p-4">mock-example</router-link>
         </div>
       </div>
     </nav>
